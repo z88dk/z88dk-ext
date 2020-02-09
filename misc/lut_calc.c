0e56a1d23eb316bfcb10601a93fd7494be427c6f
Phillip Stevens <phillip.stevens@gmail.com>
2020-02-09 13:26:54 +1100

    misc - lut_calc creation date

--- misc/lut_calc.c
+++ misc/lut_calc.c
@@ -1,5 +1,5 @@
 //
-// feilipu, 2019 April
+// feilipu, 2020 January
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
