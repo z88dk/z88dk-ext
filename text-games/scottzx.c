dd2fef65670310ddead85d3927f923e82668210d
Stefano <stefano_bodrato@hotmail.com>
2021-05-31 11:55:33 +0200

    Update scottzx.c

--- text-games/scottzx.c
+++ text-games/scottzx.c
@@ -2,9 +2,6 @@
  *	ScottFree Revision 1.14
  *	Adapted by Stefano Bodrato to run in tiny environments.
  *	
- *
- *
- *
  *	This program is free software; you can redistribute it and/or
  *	modify it under the terms of the GNU General Public License
  *	as published by the Free Software Foundation; either version
