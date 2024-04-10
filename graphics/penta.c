296fde996dabfc5981dc6f6852d76c10d3b95139
Stefano <stefano_bodrato@hotmail.com>
2024-04-10 15:23:39 +0200

    Create penta.c

---
+++ graphics/penta.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+#include <graphics.h>
+#include <lib3d.h>
+
+
+void turn (int degrees)
+{
+	if (degrees<0)
+		turn_left(-degrees);
+	else
+		turn_right(degrees);
+}
+
+
+void main( void )
+{
+  int a = 0, n = 0, i, j, A = 2, N = 10;
+  int deg = 72;
+
+  clg();
+  fwd(getmaxx()/2);
+  turn(90);
+  fwd(getmaxy()/2 - 10);
+  pen_down();
+
+ for ( i=0; i<=45; i++ ){
+   if ( ++a > A )
+     a = 1;
+   for ( j=1; j<=5+a; j++ ){
+     fwd( 20 );
+     turn( deg );
+   }
+   fwd( 20 );
+   deg = -deg;
+   turn( deg );
+   if ( ++n > N ){
+     a = -a;
+     n = 0;
+   }
+ }
+ 
+ while (getk()!=' ') {};
+}
